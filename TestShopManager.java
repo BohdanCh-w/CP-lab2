eecf5acc936f8b1bb1abda86d7a17956848d5559
BohdanCh-w <bohdanch.w@gmail.com>
2021-10-04 01:45:37 +0300

    Add all tests

--- TestShopManager.java
+++ TestShopManager.java
@@ -8,13 +8,14 @@ import java.util.Arrays;
 
 public class TestShopManager {
     private static ArrayList<MusicInstrument> getTestList() {
-        ArrayList<MusicInstrument> ret = new ArrayList<MusicInstrument>();
-        ret.add(new Horn("H-284", "Sony", 7.95, 3.7, false, 'C'));
-        ret.add(new Trumpet("T-624", "Trumpy", 44.65, 2.7, true, "style-1"));
-        ret.add(new Cymbal("C-435", "Sara", 21.95, 2.8, 440));
-        ret.add(new Drum("D-342", "Sony", 31.4, 23.1, 5));
-        ret.add(new Guitar("G-512", "Infinite", 220.0, 6.8, 6, "style-a"));
-        ret.add(new Violin("V-942", "Sara", 572, 3.1, 4, "hourse"));
+        ArrayList<MusicInstrument> ret = new ArrayList<MusicInstrument>(Arrays.asList(
+            new Horn("H-284", "Sony", 7.95, 3.7, false, 'C'),
+            new Trumpet("T-624", "Trumpy", 44.65, 2.7, true, "style-1"),
+            new Cymbal("C-435", "Sara", 21.95, 2.8, 440),
+            new Drum("D-342", "Sony", 31.4, 23.1, 5),
+            new Guitar("G-512", "Infinite", 220.0, 6.8, 6, "style-a"),
+            new Violin("V-942", "Sara", 572, 3.1, 4, "hourse")
+        ));
         return ret;
     }
 
@@ -37,7 +38,94 @@ public class TestShopManager {
         return ret1.equals(exp1);
     }
 
+    public static boolean TestFindMusicInstrumentForBand() {
+        ArrayList<MusicInstrument> testList = getTestList();
+        MusicShopManager manager = new MusicShopManager();
+        
+        var ret1 = manager.FindMusicInstrumentForBand(testList);
+        ArrayList<MusicInstrument> exp1 = new ArrayList<MusicInstrument>(Arrays.asList(
+            new Trumpet("T-624", "Trumpy", 44.65, 2.7, true, "style-1"),
+            new Drum("D-342", "Sony", 31.4, 23.1, 5),
+            new Guitar("G-512", "Infinite", 220.0, 6.8, 6, "style-a")
+        ));
+
+        return ret1.equals(exp1);
+    }
+
+    public static boolean TestSortCode() {
+        ArrayList<MusicInstrument> testList = getTestList();
+        MusicShopManager manager = new MusicShopManager();
+        
+        var ret1 = manager.SortCode(testList, false);
+        ArrayList<MusicInstrument> exp1 = new ArrayList<MusicInstrument>(Arrays.asList(
+            new Cymbal("C-435", "Sara", 21.95, 2.8, 440),
+            new Drum("D-342", "Sony", 31.4, 23.1, 5),
+            new Guitar("G-512", "Infinite", 220.0, 6.8, 6, "style-a"),
+            new Horn("H-284", "Sony", 7.95, 3.7, false, 'C'),
+            new Trumpet("T-624", "Trumpy", 44.65, 2.7, true, "style-1"),
+            new Violin("V-942", "Sara", 572, 3.1, 4, "hourse")
+        ));
+
+        return ret1.equals(exp1);
+    }
+
+    public static boolean TestSortProducer() {
+        ArrayList<MusicInstrument> testList = getTestList();
+        MusicShopManager manager = new MusicShopManager();
+        
+        var ret1 = manager.SortProducer(testList, true);
+        ArrayList<MusicInstrument> exp1 = new ArrayList<MusicInstrument>(Arrays.asList(
+            new Trumpet("T-624", "Trumpy", 44.65, 2.7, true, "style-1"),
+            new Drum("D-342", "Sony", 31.4, 23.1, 5),
+            new Horn("H-284", "Sony", 7.95, 3.7, false, 'C'),
+            new Violin("V-942", "Sara", 572, 3.1, 4, "hourse"),
+            new Cymbal("C-435", "Sara", 21.95, 2.8, 440),
+            new Guitar("G-512", "Infinite", 220.0, 6.8, 6, "style-a")
+        ));
+
+        return ret1.equals(exp1);
+    }
+
+    public static boolean TestSortPrice() {
+        ArrayList<MusicInstrument> testList = getTestList();
+        MusicShopManager manager = new MusicShopManager();
+        
+        var ret1 = manager.sortByPrice(testList, true);
+        ArrayList<MusicInstrument> exp1 = new ArrayList<MusicInstrument>(Arrays.asList(
+            new Violin("V-942", "Sara", 572, 3.1, 4, "hourse"),
+            new Guitar("G-512", "Infinite", 220.0, 6.8, 6, "style-a"),
+            new Trumpet("T-624", "Trumpy", 44.65, 2.7, true, "style-1"),
+            new Drum("D-342", "Sony", 31.4, 23.1, 5),
+            new Cymbal("C-435", "Sara", 21.95, 2.8, 440),
+            new Horn("H-284", "Sony", 7.95, 3.7, false, 'C')
+        ));
+
+        return ret1.equals(exp1);
+    }    
+
+    public static boolean TestSortWeight() {
+        ArrayList<MusicInstrument> testList = getTestList();
+        MusicShopManager manager = new MusicShopManager();
+        
+        var ret1 = manager.sortByWeight(testList, false);
+        ArrayList<MusicInstrument> exp1 = new ArrayList<MusicInstrument>(Arrays.asList(
+            new Trumpet("T-624", "Trumpy", 44.65, 2.7, true, "style-1"),
+            new Cymbal("C-435", "Sara", 21.95, 2.8, 440),
+            new Violin("V-942", "Sara", 572, 3.1, 4, "hourse"),
+            new Horn("H-284", "Sony", 7.95, 3.7, false, 'C'),
+            new Guitar("G-512", "Infinite", 220.0, 6.8, 6, "style-a"),
+            new Drum("D-342", "Sony", 31.4, 23.1, 5)
+        ));
+
+        return ret1.equals(exp1);
+    }
+
     public static void main(String[] args) {
         System.out.println(TestFindMusicInstrumentByType());
+        System.out.println(TestFindMusicInstrumentForBand());
+        System.out.println(TestSortCode());
+        System.out.println(TestSortProducer());
+        System.out.println(TestSortPrice());
+        System.out.println(TestSortWeight());
     }
 }
